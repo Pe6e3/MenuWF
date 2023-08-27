18ce19ab19525920720f0d82321caefa4284c474
Anton <xpe6e3@gmail.com>
2023-08-27 17:07:10 +0600

    Добавил Header к форме

--- MenuWF/UIElements/EgoldsFormStyle.cs
+++ MenuWF/UIElements/EgoldsFormStyle.cs
@@ -34,7 +34,11 @@ namespace MenuWF.UIElements
         private Color HeaderColor = Color.DimGray;
         private StringFormat SF = new StringFormat();
         private Font Font = new Font("Arial", 9F, FontStyle.Regular);
+        private Size IconSize = new Size(14, 14);
 
+        private bool MousePressed = false;
+        private Point clickPosition;     // позиция курсора в момент клика
+        private Point moveStartPosition; // позция формы в момент клика
 
         #endregion
 
@@ -65,7 +69,36 @@ namespace MenuWF.UIElements
             Form.FormBorderStyle = FormBorderStyle.None;
             OffSetControls();
             Form.Paint += FormPaint;
+
+            Form.MouseDown += Form_MouseDown;
+            Form.MouseUp += Form_MouseUp;
+            Form.MouseMove += Form_MouseMove;
         }
+
+        private void Form_MouseUp(object? sender, MouseEventArgs e)
+        {
+            MousePressed = false;
+
+        }
+        private void Form_MouseDown(object? sender, MouseEventArgs e)
+        {
+            if (e.Location.Y <= HeaderHeight)
+            {
+                MousePressed = true;
+                clickPosition = Cursor.Position;
+                moveStartPosition = Form.Location;
+            }
+        }
+
+        private void Form_MouseMove(object? sender, MouseEventArgs e)
+        {
+            if (MousePressed)
+            {
+                Size frmOffset = new Size(Point.Subtract(Cursor.Position, new Size(clickPosition)));
+                Form.Location = Point.Add(moveStartPosition, frmOffset);
+            }
+        }
+
 
         private void FormPaint(object? sender, PaintEventArgs e)
         {
@@ -75,10 +108,15 @@ namespace MenuWF.UIElements
         private void DrawStyle(Graphics graph)
         {
             graph.SmoothingMode = SmoothingMode.HighQuality;
-            Rectangle rectHeader = new Rectangle(0, 0, Form.Width - 1, HeaderHeight);
-            Rectangle rectBorder = new Rectangle(0, 0, Form.Width - 1, Form.Height - 1);
+            Rectangle rectHeader = new Rectangle(0, 0, Form.Width - 1, HeaderHeight);     // Заголовок формы
+            Rectangle rectBorder = new Rectangle(0, 0, Form.Width - 1, Form.Height - 1);  // Обводка заголовка
+            Rectangle rectTitleText = new Rectangle(rectHeader.X + 30, rectHeader.Y, rectHeader.Width, rectHeader.Height); // Текст названия окна
+            Rectangle rectIcon = new Rectangle(
+                rectHeader.Height / 2 - IconSize.Width / 2,
+                rectHeader.Height / 2 - IconSize.Height / 2,
+                IconSize.Width, IconSize.Height
+                );
 
-            Rectangle rectTitleText = new Rectangle(rectHeader.X +30, rectHeader.Y, rectHeader.Width, rectHeader.Height);
             // Шапка
             graph.DrawRectangle(new Pen(HeaderColor), rectHeader);
             graph.FillRectangle(new SolidBrush(HeaderColor), rectHeader);
@@ -88,7 +126,12 @@ namespace MenuWF.UIElements
 
             // Текст заголовка
             graph.DrawString(Form.Text, Font, new SolidBrush(Color.White), rectTitleText, SF);
+
+            // Иконка
+            graph.DrawImage(Form.Icon.ToBitmap(), rectIcon);
+
         }
+
 
         private void OffSetControls()
         {
